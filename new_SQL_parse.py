c43962f24b306302c13574876eec4807712d1c96
Ryan Kush <ryankush131@gmail.com>
2015-07-27 12:58:56 -0500

    Changed calculations for LOC and added offline support

--- new_SQL_parse.py
+++ new_SQL_parse.py
@@ -13,11 +13,44 @@ import json  # Used to read JSON file from query
 import os  # Used to execute command line commands in python.
 import sqlite3  # Used to put data into the database
 import time  # Used to sleep script for 5 seconds
+import math  # Used to round up numbers
 from query import query  # JIRA query file
 
 
 import pprint
 pp = pprint.PrettyPrinter(depth=6)
+
+
+def collectLines(new_file, project_name, jiraNew, jiraDefects, lc_add, lc_sub):
+    """Collect total lines changed for a JIRA issue."""
+    data = {}
+    with open(new_file) as f:
+        for line in f:
+            total_add = 0
+            total_sub = 0
+            keys_found = 0
+            data.update(json.loads(line))
+            if 'trackingIds' not in data:
+                continue
+            else:
+                for x in data['trackingIds']:
+                    g_key = x['id']
+                    for x in (jiraNew + jiraDefects):
+                        if x['key'] == g_key:
+                            keys_found += 1
+                            s = len(x)
+                            for info in data['patchSets']:
+                                total_add += info['sizeInsertions']/s
+                                total_sub += info['sizeDeletions']/s
+                    if g_key in lc_add and keys_found != 0:
+                        lc_add[g_key] += total_add
+                        lc_sub[g_key] += total_sub
+                    elif keys_found != 0:
+                        lc_add[g_key] = total_add
+                        lc_sub[g_key] = total_sub
+                    else:
+                        continue
+    f.close()
 
 
 def jiraCollect(x, a_m, s_m):
@@ -77,7 +110,8 @@ def jiraCollect(x, a_m, s_m):
     return results_list
 
 
-def addToDB(filename, project_name, db, jira, keyExists, new_or_defect):
+def addToDB(filename, project_name, db, jira, new_or_defect,
+            key_count, lc_add, lc_sub, key_exist):
     """Add query information to database."""
     # Metric variables.
     insert = 0  # How many inserts we process
@@ -96,49 +130,46 @@ def addToDB(filename, project_name, db, jira, keyExists, new_or_defect):
             #  If we can't find a JIRA associated with this gerrit patch, skip
             #  it. We have no purpose for this data.
             if 'trackingIds' not in data:
-                pass
+                continue
 
             #  If we can find an associated JIRA in our patch set, we can
             #  begin to extract the relevant data for insertion into the
             #  database.
             else:
-                do_want = True  # We start out wanting this JIRA in our set.
-                total_add = 0  # How many insertions from the gerrit patch?
-                total_sub = 0  # How many deletions from this gerrit patch?
-
-                #  Add up the lines of code for all the patches in the
-                for info in data['patchSets']:
-                    total_add += info['sizeInsertions']
-                    total_sub += info['sizeDeletions']
-
-                #  If we find that we have no lines changed, go to the next
-                #  gerrit patch, this is a useless patch.
-                if total_add == 0 and total_sub == 0:
-                    continue
-
-                #  Calculate the effort modifiers for lines of code.
-                #  Allows us to differentiate how much effort should
-                #  go to new/modified or defect/modified.
-                add_modifier = total_add / (total_add + abs(total_sub))
-                sub_modifier = total_sub / (total_add + abs(total_sub))
-
-                #  Divide the LoC by the number of issues in the gerrit patch.
-                #  Allows for us to give a more proper estimate by giving
-                #  divided up LoC per JIRA patch.
-                total_add = total_add / len(data['trackingIds'])
-                total_sub = total_sub / len(data['trackingIds'])
-
-                #  Search through our JIRA patches for matching patches that
-                #  were inside of our gerrit patch.
                 for issue in data['trackingIds']:
-                    total_issues = len(data['trackingIds'])
-                    new_total_a = total_add/total_issues
-                    new_total_s = total_sub/total_issues
-                    gerrit_key = issue['id']
+                    do_want = True  # We start out wanting this JIRA in our set
+                    total_add = 0  # How many insertions from the gerrit patch?
+                    total_sub = 0  # How many deletions from this gerrit patch?
+                    gerrit_key = issue['id']  # Define the JIRA in the gerrit.
+
+                    if gerrit_key in key_exist:
+                        continue
+
+                    if gerrit_key not in lc_add:
+                        continue
+                    else:
+                        total_add = math.ceil(lc_add[gerrit_key])
+                        total_sub = math.ceil(lc_sub[gerrit_key])
+
+                    #  If we find that we have no lines changed, go to the next
+                    #  gerrit patch, this is a useless patch.
+                    if total_add == 0 and total_sub == 0:
+                        continue
+
+                    #  Calculate the effort modifiers for lines of code.
+                    #  Allows us to differentiate how much effort should
+                    #  go to new/modified or defect/modified.
+                    add_modifier = total_add / (total_add + abs(total_sub))
+                    sub_modifier = total_sub / (total_add + abs(total_sub))
+
                     for x in jira:
                         jira_key = x['key']
                         # Found a match? Fetch our info from the JIRA
                         if gerrit_key == jira_key:
+                            if jira_key in key_count:
+                                key_count[jira_key] += 1
+                            else:
+                                key_count[jira_key] = 1
                             do_want = True
                             jira_payload = jiraCollect(x,
                                                        add_modifier,
@@ -148,8 +179,8 @@ def addToDB(filename, project_name, db, jira, keyExists, new_or_defect):
                             # appropriate variable that we want to store the
                             # data into.
                             do_want = jira_payload.pop()
-                            sub_estimatedEffort = jira_payload.pop()
-                            add_estimatedEffort = jira_payload.pop()
+                            sub_estimatedEffort = math.ceil(jira_payload.pop())
+                            add_estimatedEffort = math.ceil(jira_payload.pop())
                             comp = jira_payload.pop()
                             reporter = jira_payload.pop()
                             assignee = jira_payload.pop()
@@ -168,40 +199,42 @@ def addToDB(filename, project_name, db, jira, keyExists, new_or_defect):
                                     s = "insert into estimates " + \
                                         "VALUES(\'NEW\'," + \
                                         str(abs(add_estimatedEffort)) + "," + \
-                                        str(abs(new_total_a)) + \
+                                        str(abs(total_add)) + \
                                         ",\'" + assignee + \
                                         "\',\'" + reporter + "\',\'" + comp + \
                                         "\',\'" + jira_key + "\')"
                                     print(s)
+                                    key_exist[jira_key] = 1
                                 else:
                                     s = "insert into estimates " + \
                                         "VALUES(\'DEFECTS\'," + \
                                         str(abs(add_estimatedEffort)) + "," + \
-                                        str(abs(new_total_a)) + \
+                                        str(abs(total_add)) + \
                                         ",\'" + assignee + \
                                         "\',\'" + reporter + "\',\'" + comp + \
                                         "\',\'" + jira_key + "\')"
                                     print(s)
+                                    key_exist[jira_key] = 1
 
                                 conn.execute(s)
                                 conn.commit()
                                 s = "insert into estimates " + \
                                     "VALUES(\'MODIFIED\'," + \
                                     str(abs(sub_estimatedEffort)) + "," + \
-                                    str(abs(new_total_s)) + \
+                                    str(abs(total_sub)) + \
                                     ",\'" + assignee + \
                                     "\',\'" + reporter + "\',\'" + comp + \
                                     "\',\'" + jira_key + "\')"
                                 print(s)
+                                key_exist[jira_key] = 1
                                 insert += 1
                                 conn.execute(s)
-                                keyExists[data['trackingIds'][0]['id']] = 1
                             break
                         else:
                             continue
 
-    print(filename + " Summary:\n")
-    print("\nTotal of " + str(insert) + " rows were inserted\n " +
+    print(filename + " Summary:")
+    print("\nTotal of " + str(insert) + " rows were inserted\n" +
           "Total of " + str(duplicate_jira) + " have keys in database.")
 
     #  "Total of " + str(noMatchingKey) +
@@ -227,6 +260,8 @@ print(PROJ_NUMBER + " projects detected to scan\n\nIf this is incorrect, " +
       "\nExecution will pause for 5 seconds, then begin.\n")
 
 time.sleep(5)  # Give user a chance to cancel command if error is detected.
+old_data = True  # Use old JSON files (1) or collect new ones (0)
+debug = False
 
 for _ in range(int(PROJ_NUMBER)):
 
@@ -251,10 +286,20 @@ for _ in range(int(PROJ_NUMBER)):
     filename = project_name.rstrip('\n')
     filename = filename.replace('/', '')
 
-    print("\nCollecting NEW Queries...\n")
-    jiraNew = query(jira_new)
-    print("\nCollecting DEFECT Queries...\n")
-    jiraDefects = query(jira_defects)
+    if old_data is False:
+        print("\nCollecting NEW Queries...\n")
+        jiraNew = query(jira_new)
+        with open('jiraNew.json', 'w') as fp:
+            json.dump(jiraNew, fp)
+        print("\nCollecting DEFECT Queries...\n")
+        jiraDefects = query(jira_defects)
+        with open('jiraDefects.json', 'w') as fp:
+            json.dump(jiraDefects, fp)
+    else:
+        with open('jiraNew.json', 'r') as fp:
+            jiraNew = json.load(fp)
+        with open('jiraDefects.json', 'r') as fp:
+            jiraDefects = json.load(fp)
 
     total_new_JIRA = 0  # How many new JIRA's we process.
     total_defects_JIRA = 0  # How many defect JIRA's we process.
@@ -280,21 +325,13 @@ for _ in range(int(PROJ_NUMBER)):
     # Initial setup of database table
     conn.close()
 
-    # A really bad way to get how many JIRA's we have from our query.
-    for x in jiraNew:
-        total_new_JIRA += 1
+    key_exist = {}
+    key_count = {}  # Does the JIRA issue already exist?
+    lc_add = {}
+    lc_sub = {}
 
-    # x['fields']['assignee']['name']
-    # x['fields']['reporter']['name']
-    # x['fields']['components'][0]['name']
-
-    for x in jiraDefects:
-        total_defects_JIRA += 1
-
-    keyExists = {}  # Does the JIRA issue already exist?d
-
-    print(total_new_JIRA)
-    print(total_defects_JIRA)
+    total_new_JIRA = len(jiraNew)
+    total_defects_JIRA = len(jiraDefects)
 
     queryTotal = 0
     while queryTotal < total_new_JIRA + total_defects_JIRA:
@@ -312,15 +349,31 @@ for _ in range(int(PROJ_NUMBER)):
             " status:merged --format=json --patch-sets --files" + \
             " --submit-records -S " + str(queryTotal) + " > " + new_file
 
-        # Print out our terminal command just as a syntax check for user.
-        print(term_command)
-
         # Execute our terminal command that we created above. At this point,
         # we have a valid .JSON file with all of our project data without the
-        # time estimates. We will pull this info from JIRA later.
-        os.system(term_command)
-        addToDB(new_file, project_name, db_filename, jiraNew, keyExists, 0)
-        addToDB(new_file, project_name, db_filename, jiraDefects, keyExists, 1)
+        # time estimates. We will pull this info from JIRA later. Also looks
+        # at old data flag to avoid time wastes.
+
+        if old_data is False:
+            print(term_command)
+            os.system(term_command)
+
+        collectLines(new_file, project_name, jiraNew,
+                     jiraDefects, lc_add, lc_sub)
+
         queryTotal += 500
 
+    queryTotal = 0
+    while queryTotal < total_new_JIRA + total_defects_JIRA:
+        new_file = filename + str(queryTotal) + ".json"
+        addToDB(new_file, project_name, db_filename,
+                jiraNew, 0, key_count, lc_add, lc_sub, key_exist)
+        addToDB(new_file, project_name, db_filename,
+                jiraDefects, 1, key_count, lc_add, lc_sub, key_exist)
+        queryTotal += 500
+
+if debug is True:
+    for x in key_count:
+        print(x)
+        print(key_count[x])
 # pylama:ignore=C901, E501
